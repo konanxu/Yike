4b2e04d88aaff75717e79ebfea7554d083571cbe
Konan <konanxuxi@gmail.com>
2017-09-09 17:33:56 +0800

    添加gulp

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+/**
+ * Created by konan on 17/9/9.
+ */
+
+//引入gulp
+
+var gulp = require('gulp');
+
+//定义任务
+gulp.task('less',function () {
+    console.log('gulp study！！');
+    gulp.src('./public/')
+});
--- scripts/controllers.js
+++ scripts/controllers.js
@@ -3,26 +3,54 @@
  */
 
 //实例一个模块 ，用来管理所有控制器
-angular.module('Controllers',[])
+angular.module('Controllers', [])
 
-.controller('DemoController',['$scope',function ($scope) {
-    console.log("ss");
-}])
+    .controller('DemoController', ['$scope', function ($scope) {
+        console.log("ss");
+    }])
 
-//导航菜单
-.controller('NavController',['$scope',function($scope){
-    $scope.navs = [
-        {link: '#/today',text:"今日一刻",icon: "icon-home"},
-        {link: '#older',text: '往期内容' ,icon: "icon-file-empty"}
-    ];
-}])
+    //导航菜单
+    .controller('NavController', ['$scope', function ($scope) {
+        $scope.navs = [
+            {link: '#/today', text: "今日一刻", icon: "icon-home"},
+            {link: '#/older', text: '往期内容', icon: "icon-file-empty"}
+        ];
+    }])
 
-.controller('TodatController',['$scope','$http',function ($scope, $http) {
-    $http({
-        url:'./api/today.php',
-        method:'get'
-    }).success(function (info) {
-        $scope.posts = info.posts;
-    })
-}])
+    .controller('TodayController', ['$scope', '$http', '$filter', '$rootScope', function ($scope, $http, $filter, $rootScope) {
+        var today = $filter('date')(new Date, 'yyyy-MM-dd');
+
+        $rootScope.title = '今日一刻';
+        $rootScope.index = 0;
+        $rootScope.loaded = false;
+
+        $http({
+            url: './api/today.php',
+            method: 'get',
+            params:{today:today}
+        }).then(function (success) {
+            console.log(success);
+            $rootScope.loaded = true;
+            $scope.date = success.data.date;
+            $scope.posts = success.data.posts;
+        }, function (error) {
+            console.log(error);
+        });
+    }])
+
+    .controller('OlderController',['$scope','$http','$filter','$rootScope',function($scope,$http,$filter,$rootScope){
+        $http({
+            url:'./api/older.php',
+            method:'get',
+            // params:{}
+        }).then(function(success){
+            console.log(success);
+            $rootScope.loaded = true;
+            $scope.date = success.data.date;
+            $scope.posts = success.data.posts;
+        }, function(error){
+
+        });
+    }])
+    
 
