df7877f63e16a335a0d76c4f3247a514130d0aa2
Konan <konanxuxi@gmail.com>
2017-09-11 20:58:35 +0800

    添加gulp插件

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,20 @@
 //引入gulp
 
 var gulp = require('gulp');
-
+var sass = require('gulp-sass');
+var csso = require('gulp-csso');
+var htmlminify = require('gulp-html-minify');
 //定义任务
-gulp.task('less',function () {
-    console.log('gulp study！！');
-    gulp.src('./public/')
+gulp.task('cssConfig',function () {
+    // console.log('gulp study！！');
+    return gulp.src('./public/sass/*.scss')
+        .pipe(sass().on('error',sass.logError))
+        .pipe(csso())
+        .pipe(gulp.dest('./public/css'));
 });
+
+gulp.task('htmlConfig',function () {
+    return gulp.src('./index.html')
+        .pipe(htmlminify())
+        .pipe(gulp.dest('./release'));
+})
